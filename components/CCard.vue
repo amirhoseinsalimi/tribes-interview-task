f3029d65ff0b9a95a171a70c12152924e89a22f2
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-01 01:21:31 +0430

    feat(CCard): implement "CCard" component

---
+++ components/CCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="shadow px-3 rounded-xl">
+    <div class="border-b border-gray-400 pb-2 mx-3">
+      <slot name="header"></slot>
+    </div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CCard'
+};
+</script>
