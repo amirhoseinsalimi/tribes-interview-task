75e04985101eb0d7a42525c942d116c5de268adf
Amir Hosein Salimi <a.salimi@karboom.io>
2022-03-31 19:16:53 +0430

    feat(tailwind): add "tailwind.config.js" files

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+        '4.5xl': '2.5rem',
+      },
+      fontSize: {
+        '1.5xl': '1.375rem',
+        '4.5xl': '2.5rem',
+      },
+      boxShadow: {
+        'inner-2xl': 'inset 0 10px 8px 0 rgb(0 0 0 / 0.05)',
+      },
+      colors: {
+        primary: '#01a8d5',
+        secondary: '#616161',
+        tertiary: '#fe878f',
+        flame: '#fe878f',
+        white: '#fff',
+        black: '#000',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+}
