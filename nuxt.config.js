1fdcb34eae70aed0da36ca97ee6a100e58a55981
Amir Hosein Salimi <a.salimi@karboom.io>
2022-03-31 20:34:12 +0430

    fix: configure axios base URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,13 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseUrl: process.env.API_URL,
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.API_URL,
+    },
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
