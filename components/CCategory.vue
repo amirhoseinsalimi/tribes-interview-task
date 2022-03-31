cdb4b01309842f06381dac11021da98f28b5b6c2
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-01 01:21:47 +0430

    feat(CCategory): implement "CCategory" component

---
+++ components/CCategory.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="items.length">
+    <CCard class="py-6">
+      <template #header>
+        <CCheckbox
+          :id="title.id"
+          v-model="allSelected"
+          :label="title.title"
+          @checked="destroy"
+          @unchecked="unCheckAllItems"
+        />
+      </template>
+
+      <template #default>
+        <ul class="list-none pt-4 pl-8">
+          <li v-for="item in items" :key="item.id">
+            <CCheckbox :id="item.id" v-model="item.value" :label="item.title" />
+          </li>
+        </ul>
+      </template>
+    </CCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CCategory',
+  props: {
+    title: {
+      type: Object,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      allSelected: this.title.value,
+    }
+  },
+  methods: {
+    destroy() {
+      this.checkAllItems()
+
+      this.$emit('destroy', this.title.id)
+    },
+    checkAllItems() {
+      this.items.forEach((item) => (item.value = true))
+    },
+    unCheckAllItems() {
+      this.items.forEach((item) => (item.value = false))
+    },
+  },
+}
+</script>
