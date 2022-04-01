a572b55c5bd9a2bd51443ba4d79398ffef48f37d
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-01 15:39:16 +0430

    test(CChip): write unit test

---
+++ test/unit/CChip.spec.js
@@ -0,0 +1,42 @@
+import { mount } from '@vue/test-utils';
+import CChip from '~/components/CChip';
+
+describe('CChip', () => {
+  const id = 2;
+  const title = 'Chip Text';
+  const type = 'secondary';
+
+  test('should render `title` prop', () => {
+    const wrapper = mount(CChip, {
+      propsData: {
+        id,
+        title,
+      },
+    });
+
+    expect(wrapper.html()).toContain(title);
+  });
+
+  test('should render `id` prop', () => {
+    const wrapper = mount(CChip, {
+      propsData: {
+        id,
+        title,
+      },
+    });
+
+    expect(wrapper.html()).toContain(`${id}`);
+  });
+
+  test('should render secondary type', () => {
+    const wrapper = mount(CChip, {
+      propsData: {
+        id,
+        title,
+        type,
+      },
+    });
+
+      expect(wrapper.classes()).toEqual(expect.arrayContaining([`text-${type}`]));
+  });
+});
