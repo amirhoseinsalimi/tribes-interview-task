a847128472559db1c8323f350f95ec846ed80886
Amir Hosein Salimi <a.salimi@karboom.io>
2022-04-01 11:30:41 +0430

    feat(CSection): implement "CSection" component

---
+++ components/CSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="border: 1px solid red;" class="w-full">
+    <div class="font-bold text-xl py-4">
+      <slot name="title"></slot>
+    </div>
+
+    <div class="px-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CSection'
+};
+</script>
