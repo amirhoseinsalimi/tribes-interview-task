136810817efbc0a2652a087f0acc86046d8b0523
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-04-05 00:20:25 +0430

    feat: implement `UtilService.addField` method

--- services/util.service.js
+++ services/util.service.js
@@ -4,4 +4,10 @@ export class UtilService extends BaseService {
   generateRandomNumber(max) {
     return Math.floor(Math.random() * max)
   }
+
+  addField(target, fieldName, value) {
+    target[fieldName] = value
+
+    return target
+  }
 }
